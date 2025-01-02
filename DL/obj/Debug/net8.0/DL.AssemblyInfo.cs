716199c6ae9116136bb70dab1eaee15570ef1918
heo heo <mc@gmail.com>
2025-01-02 14:25:37 +0700

    Up lại nè

--- DL/obj/Debug/net8.0/DL.AssemblyInfo.cs
+++ DL/obj/Debug/net8.0/DL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d542a1745596daef9337fa3858f7abaaaee67b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0f93771b860fcfaea895f97c6963a23fbcd9ac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
